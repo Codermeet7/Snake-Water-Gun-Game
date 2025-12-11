6d382bb1dd2bfb44719b8af7d323864a3b617f75
Meet Gadge <meetgadge@gmail.com>
2025-12-11 18:40:37 +0530

    CODE INFO

--- README.py
+++ README.py
@@ -1,2 +1,3 @@
 # Snake-Water-Gun-Game
 🚀 I created a Python Mini Project – Snake Water Gun Game!  I built this game using basic Python concepts like: ✔ Random module ✔ Conditional statements ✔ User input ✔ Simple game logic
+To run this program copy and paste the program on any compiler like vs code and etc.
